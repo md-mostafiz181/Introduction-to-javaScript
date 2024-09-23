b8aa7a8c78db5ef04df932648498241d322dc634
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-09-23 09:02:31 +0600

    practice different types of number

---
+++ number.js
@@ -0,0 +1,9 @@
+var applePrice =20;
+var orangePrice=parseInt("30");
+
+console.log(applePrice + orangePrice)
+
+var mangoPrice=0.1;
+var grapePrice=0.2;
+var total = mangoPrice + grapePrice ;
+console.log(parseFloat(total.toFixed(2)))
