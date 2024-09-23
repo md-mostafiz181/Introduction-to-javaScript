c3f8b191dd25de010eea50d2b17c2fbae434abfa
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-09-23 10:06:20 +0600

    practice operation shorthand

---
+++ shorthand.js
@@ -0,0 +1,18 @@
+var price = 30 ;
+// price = price + 10 ;
+price += 10  //short hand
+console.log(price)
+
+var money = 20 ;
+money -= 10 ;
+console.log(money)
+
+
+var applePrice = 30 ;
+applePrice *= 3 ;
+console.log(applePrice)
+
+var mangoes = 100 ;
+// mangoes /= 3 ;
+mangoes %= 3 ;
+console.log(mangoes)
