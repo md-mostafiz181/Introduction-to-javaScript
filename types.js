a0b67963d262f54c24f0a9a027731f406af779bb
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-09-23 08:33:09 +0600

    different types of variables

---
+++ types.js
@@ -0,0 +1,16 @@
+// string type
+var country = "Bangladesh" ;
+var name = " a front end developer";
+console.log(country, name)
+console.log(typeof country, name)
+
+// number 
+var price = 34;
+console.log(price)
+console.log(typeof price)
+
+// boolean
+
+var isRich = true;
+var isPassed= true;
+console.log(typeof isRich)
