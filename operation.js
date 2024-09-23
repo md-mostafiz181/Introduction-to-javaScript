b1c8fcedfffdcc27fdf9066257a94472b6a3708c
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-09-23 09:35:51 +0600

    practice mathmatical operation

---
+++ operation.js
@@ -0,0 +1,20 @@
+var applePrice = 10;
+var orangePrice = 20 ;
+var total = applePrice + orangePrice ;
+console.log(total)
+
+var money = 10%3 ; // vagsesh dekhar jonnog
+var money2 = 10/3 ; // vagfoll and vagsesh dekhar jonno
+console.log(money, money2)
+
+
+var price = 10 ;
+price = price + 30;
+
+console.log(price)
+
+var newPrice = price + 50 ;
+console.log(newPrice)
+
+var discountPrice = newPrice - 10 ;
+console.log(discountPrice)
