49c14c14fc25f739483618aba39a4bf538071e11
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-09-23 10:52:48 +0600

    added pratice task for more output

---
+++ practice.js
@@ -0,0 +1,33 @@
+// problem-1
+var myMoney = 1000 ;
+var applePrice = 400 ;
+var orangePrice = 300 ;
+var totalCost = applePrice + orangePrice ;
+console.log(totalCost)
+var remainder = myMoney - totalCost ;
+console.log (remainder)
+// problem-1 is solved;
+
+
+//problem -2
+
+var totalSubject = 5 ;
+
+var mathmatics = 75.25;
+var biology = 65 ;
+var chemistry = 80 ;
+var physics = 35.45 ;
+var bangla = 99.50 ;
+
+var totalNumber = mathmatics + biology + chemistry + physics + bangla ;
+console.log(totalNumber)
+
+var result = totalNumber/totalSubject;
+console.log(parseFloat(result.toFixed(2)))
+//problem-2 is solved
+
+//problem-3
+var myNumber = 119 ;
+var partner = 5 ;
+var remainder = 119%5;
+console.log(remainder)
